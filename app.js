61c29204b4f76f9de2d99d86b8447fa19e86c1a7
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-09-29 10:28:20 -0500

    added new project prompt

--- app.js
+++ app.js
@@ -1,13 +1,91 @@
 const inquirer = require('inquirer');
-inquirer
-    .prompt([
+
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type:'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input', 
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
+    // We will add a .then() to the end of the inquirer.prompt() that's returned by promptProject().
+    .then(projectData => {
+        // We need to use the push() method to place the projectData from inquirer into the new -projects- array we just created.
+        portfolioData.projects.push(projectData);
+        // we need to evaluate the user reponse whether they wish to add more projects. 
+        // this response was captured in the answer object, projectData, in the property confirmAddProject.
+        // If the user wishes to add more projects, then this condition will evaluate to true and call the promptProject(portfolioData) function.
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+            // If the user decides not to add more projects, then the condition will evaluate to false and trigger the following statement
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+});
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
